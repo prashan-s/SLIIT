a600a1d55347b01aa135c12731f9a91683538ca5
Prashan Samarathunge <prashansamarathunge@gmail.com>
2023-03-25 19:27:30 +0530

    Pass by reference.

--- Lab03/Lab03/Lab03.cpp
+++ Lab03/Lab03/Lab03.cpp
@@ -2,19 +2,72 @@
 //
 
 #include <iostream>
+#include <iomanip>
+using namespace std;
+ 
+struct Student {
+	int mark1;
+	int mark2;
+	float ca1;
+	float ca2;
+};
+
+void findMarks(int t_marks1, int t_marks2, float& t_CA1, float& t_CA2);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Student students[5] = {};
+
+	
+	for (int i = 0; i < 5; i++) {
+		Student tStu;
+		cout << "Enter Mark 1 for Student " << i+1 << ": ";
+		cin >> tStu.mark1;
+
+		cout << "Enter Mark 2 for Student " << i+1 << ": ";
+		cin >> tStu.mark2;
+
+		findMarks(tStu.mark1, tStu.mark2, tStu.ca1, tStu.ca2);
+		students[i] = tStu;
+	}
+	cout
+		<< endl
+		<< "Student"
+		<< setw(10)
+		<< "Marks1"
+		<< setw(10)
+		<< "Marks2"
+		<< setw(10)
+		<< "CA_1"
+		<< setw(10)
+		<< "CA_2"
+		<< endl;
+
+	for (int i = 0; i < 5; i++) {
+		Student tStu = students[i];
+		cout
+			<< i + 1
+			<< setw(10)
+			<< tStu.mark1
+			<< setw(10)
+			<< tStu.mark2
+			<< setw(10)
+			<< setiosflags(ios::fixed)
+			<< setprecision(2)
+			<< tStu.ca1
+			<< setw(10)
+			<< setiosflags(ios::fixed)
+			<< setprecision(2)
+			<< tStu.ca2
+			<< endl;
+
+	}
+
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void findMarks(int t_marks1, int t_marks2, float& t_CA1, float& t_CA2) {
+	t_CA1 = float(t_marks1 * 20 / 100);
+	t_CA2 = float(t_marks1 * 30 / 100);
+}
